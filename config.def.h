dba875409b37739726bffc2ec34df8f2391ea2d6
NativeRV <nativerv@protonmail.com>
2021-08-31 23:35:14 +0300

    feat(patch): add st-osc10-20210106-4ef0cbd

--- config.def.h
+++ config.def.h
@@ -132,6 +132,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"black",
+	"gray90",
 };
 
 
@@ -139,9 +141,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+
+unsigned int defaultbg = 258;
+unsigned int defaultfg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
